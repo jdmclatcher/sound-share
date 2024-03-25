89aacd11dbb7cff4940d81d4759c116f85335530
Jonathan McLatcher <jdmclatcher@gmail.com>
2024-03-25 13:24:17 -0400

    navigation screens initial

---
+++ src/screens/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to Sound Share!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
+
+export default HomeScreen;
