0f47aa265be950cb44c1cb16664a07cf7b485a89
Rishi1204 <rishinopany@gmail.com>
2024-03-27 15:43:35 +0530

    added token refresh functionality

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import { SpotifyLogin } from '../auth/SpotifyAuth';
+import { authenticate } from '../auth/SpotifyAuth';
 
 const HomeScreen = () => {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.text}>Welcome to Sound Share!</Text>
-			<SpotifyLogin />
+			<Button
+				title="Login To Spotify"
+				onPress={() => {
+					authenticate();
+				}}
+			/>
 		</View>
 	);
 };
